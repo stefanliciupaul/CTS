011928370c6aee5bc86ff5856d590176c366fc26
stefanliciupaul <liciu.stefan.paul@gmail.com>
2022-04-04 18:48:28 +0300

    ensured proper spacing between operators and operands

--- Assignment-CleanCode/src/assignment/Account.java
+++ Assignment-CleanCode/src/assignment/Account.java
@@ -2,8 +2,8 @@ package assignment;
 
 
 public class Account {
-	public double	loanValue,rate;	
-	public int	daysActive;
+	public double loanValue,rate;	
+	public int daysActive;
 	public AccountType accountType;
 	
 	public double loan() {
@@ -12,17 +12,17 @@ public class Account {
 	}
 	
 	public double getRate() {
-		System.out.println("The rate is "+rate);
+		System.out.println("The rate is " + rate);
 		return this.rate;
 	}
 	
 	//must have method - the lead has requested it in all classes
 	public double getMonthlyRate() {
-		return loanValue*rate;
+		return loanValue * rate;
 	}
 	
 	public void setValue(double value) throws Exception {
-		if(value<0)
+		if(value < 0)
 			throw new Exception();
 		else
 		{
@@ -31,7 +31,7 @@ public class Account {
 	}
 	
 	public String toString() {
-		return "Loan: "+this.loanValue+"; rate: "+this.rate+"; days active:"+daysActive+"; Type: "+accountType+";";
+		return "Loan: " + this.loanValue + "; rate: " + this.rate + "; days active:" + daysActive + "; Type: " + accountType + ";";
 	}
 	
 	public void print() {
@@ -40,21 +40,21 @@ public class Account {
 
 	public static double calculate(Account[] accounts)
 	{
-		double totalFee=0.0;
+		double totalFee = 0.0;
 		Account	account;
 		int temp = 365;
-		for	(int i=0;i<accounts.length;i++)	{
-			account=accounts[i];
-			if(account.accountType==AccountType.PREMIUM||account.accountType==AccountType.SUPER_PREMIUM)
+		for	(int i = 0; i < accounts.length; i++)	{
+			account = accounts[i];
+			if(account.accountType == AccountType.PREMIUM || account.accountType == AccountType.SUPER_PREMIUM)
 //				1.25%	broker's	fee
-				totalFee+=.0125	* ( account.loanValue*Math.pow(account.rate,(account.daysActive/365)) - account.loanValue);	//	interest-principal
+				totalFee += .0125	* ( account.loanValue * Math.pow(account.rate, (account.daysActive / 365)) - account.loanValue);	//	interest-principal
 		}
 		
 		return	totalFee;
 	}
 
 	public Account(double value, double rate, AccountType accountType) throws Exception {
-		if(value<0)
+		if(value < 0)
 			throw new Exception();
 		else
 		{

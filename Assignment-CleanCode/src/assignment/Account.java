303ce869a5ba3d5ff53622970491884c72a51422
stefanliciupaul <liciu.stefan.paul@gmail.com>
2022-04-04 17:53:07 +0300

    code formatting
    changed all variables to a single naming convention: CamelCase

--- Assignment-CleanCode/src/assignment/Account.java
+++ Assignment-CleanCode/src/assignment/Account.java
@@ -2,13 +2,13 @@ package assignment;
 
 
 public class Account {
-	public double	loan_value,rate;	
-	public int	daysActive,account_Type;
+	public double	loanValue,rate;	
+	public int	daysActive,accountType;
 	public static final int	STANDARD=0,BUDGET=1,PREMIUM=2,SUPER_PREMIUM=3;
 	
 	public double loan() {
-		System.out.println("The loan value is " + this.loan_value);
-		return loan_value;
+		System.out.println("The loan value is " + this.loanValue);
+		return loanValue;
 	}
 	
 	public double getRate() {
@@ -18,7 +18,7 @@ public class Account {
 	
 	//must have method - the lead has requested it in all classes
 	public double getMonthlyRate() {
-		return loan_value*rate;
+		return loanValue*rate;
 	}
 	
 	public void setValue(double value) throws Exception {
@@ -26,12 +26,12 @@ public class Account {
 			throw new Exception();
 		else
 		{
-			loan_value = value;
+			loanValue = value;
 		}
 	}
 	
-	public String to_string() {
-		return "Loan: "+this.loan_value+"; rate: "+this.rate+"; days active:"+daysActive+"; Type: "+account_Type+";";
+	public String toString() {
+		return "Loan: "+this.loanValue+"; rate: "+this.rate+"; days active:"+daysActive+"; Type: "+accountType+";";
 	}
 	
 	public void print() {
@@ -46,22 +46,22 @@ public class Account {
 	int temp = 365;
 	for	(int	i=0;i<accounts.length;i++)	{
 	account=accounts[i];
-	if(account.account_Type==Account.PREMIUM||account.account_Type==Account.SUPER_PREMIUM)	
+	if(account.accountType==Account.PREMIUM||account.accountType==Account.SUPER_PREMIUM)	
 	totalFee+=.0125	*	(	//	1.25%	broker's	fee
-			account.loan_value*Math.pow(account.rate,(account.daysActive/365)) - account.loan_value);	//	interest-principal
+			account.loanValue*Math.pow(account.rate,(account.daysActive/365)) - account.loanValue);	//	interest-principal
 	}
 	return	totalFee;
 	}
 
-	public Account(double value, double rate, int account_Type) throws Exception {
+	public Account(double value, double rate, int accountType) throws Exception {
 		if(value<0)
 			throw new Exception();
 		else
 		{
-			loan_value = value;
+			loanValue = value;
 		}
 		this.rate = rate;
-		this.account_Type = account_Type;
+		this.accountType = accountType;
 	}
 	
 	
